4ee09b9b839878e5ff91a5d99cd4cfebabb2cc7a
Nicholas Irving <nicholas.irving@versent.com.au>
2016-04-03 21:58:28 +1000

    Add Zulu Server

---
+++ server-zuul/src/main/java/zulserver/ZuulApplication.java
@@ -0,0 +1,20 @@
+package zulserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+
+/**
+ * * @author Spencer Gibb
+ */
+@SpringBootApplication
+@Controller
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+	}
+
+}
